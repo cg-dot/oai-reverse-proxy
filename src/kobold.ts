51e07b82f52d02fe4e582de41e3d759235642408
nai-degen <44111-khanon@users.noreply.gitgud.io>
2023-04-08 08:34:46 -0500

    removes unused files from failed rename

--- src/kobold.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const kobold = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: Implement kobold
-  res.status(501).json({ error: "Not implemented" });
-};
