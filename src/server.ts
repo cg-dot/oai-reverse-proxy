5d0184310c37b022fec5a49bb68c5b521afae169
nai-degen <44111-khanon@users.noreply.gitgud.io>
2023-04-08 02:59:43 -0500

    sets up server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,34 @@
-console.log("hi");
+import express from "express";
+import cors from "cors";
+import pino from "pino";
+import pinoHttp from "pino-http";
+
+const PORT = process.env.PORT || 7860;
+
+const app = express();
+const logger = pino();
+
+app.use(pinoHttp({ logger }));
+app.use(cors());
+app.use(
+  express.json({ limit: "10mb" }),
+  express.urlencoded({ extended: true, limit: "10mb" })
+);
+
+app.get("/", (req, res) => {
+  logger.info("Hello world");
+  res.send("hello :)");
+});
+
+app.use((err: any, _req: unknown, res: express.Response, _next: unknown) => {
+  if (err.status) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    logger.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server listening on port ${PORT}`);
+});
