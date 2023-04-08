bbfb922daba614a7a2d206c965dd051a47aabaf5
nai-degen <44111-khanon@users.noreply.gitgud.io>
2023-04-08 04:12:48 -0500

    adds auth

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+
+const PROXY_KEY = process.env.PROXY_KEY;
+
+export function auth(req: Request, res: Response, next: NextFunction) {
+  if (!PROXY_KEY) {
+    next();
+    return;
+  }
+  if (req.headers.authorization === `Bearer ${PROXY_KEY}`) {
+    next();
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+}
