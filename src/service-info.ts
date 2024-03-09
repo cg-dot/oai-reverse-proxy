96b6ea9568e53c307e154c83f68e4ed5e90d2ea1
nai-degen <khoners@protonmail.com>
2024-03-09 13:25:50 -0600

    adds azure-image endpoint to service info; hides unavailable endpoints

--- src/service-info.ts
+++ src/service-info.ts
@@ -90,13 +90,14 @@ export type ServiceInfo = {
   endpoints: {
     openai?: string;
     openai2?: string;
-    "openai-image"?: string;
     anthropic?: string;
     "anthropic-claude-3"?: string;
     "google-ai"?: string;
     "mistral-ai"?: string;
     aws?: string;
     azure?: string;
+    "openai-image"?: string;
+    "azure-image"?: string;
   };
   proompts?: number;
   tookens?: string;
@@ -149,6 +150,7 @@ const SERVICE_ENDPOINTS: { [s in LLMService]: Record<string, string> } = {
   },
   azure: {
     azure: `%BASE%/azure/openai`,
+    "azure-image": `%BASE%/azure/openai`,
   },
 };
 
@@ -216,13 +218,22 @@ function getStatus() {
 
 function getEndpoints(baseUrl: string, accessibleFamilies: Set<ModelFamily>) {
   const endpoints: Record<string, string> = {};
+  const keys = keyPool.list();
   for (const service of LLM_SERVICES) {
+    if (!keys.some((k) => k.service === service)) {
+      continue;
+    }
+
     for (const [name, url] of Object.entries(SERVICE_ENDPOINTS[service])) {
       endpoints[name] = url.replace("%BASE%", baseUrl);
     }
 
     if (service === "openai" && !accessibleFamilies.has("dall-e")) {
       delete endpoints["openai-image"];
+    }
+
+    if (service === "azure" && !accessibleFamilies.has("azure-dall-e")) {
+      delete endpoints["azure-image"];
     }
   }
   return endpoints;
