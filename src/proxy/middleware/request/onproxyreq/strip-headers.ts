d3e7ef3c14eddf359121a0311c7a97d08ff67b19
nai-degen <khoners@protonmail.com>
2024-05-01 11:26:15 -0500

    prevents leaking headers to upstream API when serving via Tailscale

--- src/proxy/middleware/request/onproxyreq/strip-headers.ts
+++ src/proxy/middleware/request/onproxyreq/strip-headers.ts
@@ -7,10 +7,15 @@ import { HPMRequestCallback } from "../index";
 export const stripHeaders: HPMRequestCallback = (proxyReq) => {
   proxyReq.setHeader("origin", "");
   proxyReq.setHeader("referer", "");
-
+  proxyReq.removeHeader("tailscale-user-login");
+  proxyReq.removeHeader("tailscale-user-name");
+  proxyReq.removeHeader("tailscale-headers-info");
+  proxyReq.removeHeader("tailscale-user-profile-pic")
   proxyReq.removeHeader("cf-connecting-ip");
   proxyReq.removeHeader("forwarded");
   proxyReq.removeHeader("true-client-ip");
   proxyReq.removeHeader("x-forwarded-for");
+  proxyReq.removeHeader("x-forwarded-host");
+  proxyReq.removeHeader("x-forwarded-proto");
   proxyReq.removeHeader("x-real-ip");
 };
